282b1ff115ba14c4c64a2e78a90c723427d366a2
robertfodor-job <79786361+robertfodor-job@users.noreply.github.com>
2022-01-02 11:13:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [displayMessage, setDisplayMessage] = useState('');
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => console.log(query), 500);
+    return () => clearTimeout(timeOutId);
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <p>{displayMessage}</p>
+    </>
   );
 }
 
